022161a33216b9f4db43bd535c952421c63e7386
akbarsazish <sazishaliakbar@gmail.com>
2024-02-28 08:44:55 +0330

    add test with  genesis block

--- block.js
+++ block.js
@@ -1,15 +1,16 @@
+const {GENESIS_DATA} = require('./config');
 
-class Block{
-    constructor({timestamp, lastHash, hash, data}){
-        this.timestamp = timestamp;
-        this.hash = hash;
-        this.lastHash = lastHash;
-        this.data = data;
-    }
+class Block {
+  constructor({ timestamp, lastHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  static genesis(){
+    return new this(GENESIS_DATA);
+  }
 }
 
-const block1 = new Block({
-    timestamp:"98797", lastHash:"lasthasn", hash:"rwewerew", data:"data"
-});
-
-console.log(block1)
+module.exports = Block;
---
+++ config.js
@@ -0,0 +1,15 @@
+const INITIAL_DIFFICULTY = 3;
+const MINE_RATE = 1000;
+const STARTING_BALANCE = 5000;
+
+const REWARD_INPUT = {address: '*authorized-reward-address*'};
+const MINING_REWARD = 100; 
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: '--@--!--',
+    hash: 'hash-one',
+    data: []
+  }
+  
+  module.exports = {GENESIS_DATA};
