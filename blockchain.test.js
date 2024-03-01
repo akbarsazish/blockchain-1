f16df7c163d53bd02b41dc71a54a9d0a6b7bb96f
akbarsazish <sazishaliakbar@gmail.com>
2024-03-01 12:46:57 +0330

    write test for chain validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,10 @@ const Block = require('./block');
 const Blockchain = require('./blockchain');
 
 describe("Blockchain{}", ()=> {
-    const blockchain = new Blockchain();
+      let blockchain = new Blockchain();
+    beforeEach(()=> {
+        blockchain = new Blockchain();
+    })
    it("contain `chain` Array instance", ()=> {
         expect(blockchain.chain instanceof Array).toBe(true)
    });
@@ -15,5 +18,41 @@ describe("Blockchain{}", ()=> {
     const newData = 'foo bar';
     blockchain.addBlock({data: newData});
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+   });
+
+   describe("isValidChain()", ()=> {
+     beforeEach(()=> {
+        blockchain.addBlock({data: "one"})
+        blockchain.addBlock({data: "two"})
+        blockchain.addBlock({data: "three"})
+     });
+
+     describe("when the chain doesn't start with genesis block", ()=> {
+        it("return false", ()=> {
+            blockchain.chain[0] = {data:"fake genesis"}
+            expect(Block.isValidation(blockchain.chain)).toEqual(false);
+        })
+     });
+
+     describe("when the chain start with genesis block", ()=> {
+        describe("and lastHash references has changed", ()=> {
+            it("return false", ()=> {
+                blockchain.chain[2].lastHash = 'broken-lasthash';
+                expect(Block.isValidation(blockchain.chain)).toEqual(false);
+            });
+        });
+        
+        describe("and the chain contains a block with invalid field", ()=> {
+            it("return false", ()=> {
+                expect(Block.isValidation(blockchain.chain)).toEqual(false);
+            })
+        });
+
+        describe("and the chain is valid", ()=> {
+            it("return true", ()=> {
+                expect(Block.isValidation(blockchain.chain)).toEqual(true);
+            })
+        });
+     });
    })
 })
